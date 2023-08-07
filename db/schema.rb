a68b54f47fae198764f29743edbb0bb7a88c83d3
Ayesha Padrawala <apadrawala5@gmail.com>
2023-08-07 11:53:11 +0530

    Added authenciation in backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_05_132044) do
+ActiveRecord::Schema.define(version: 2023_08_07_060951) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_08_05_132044) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
